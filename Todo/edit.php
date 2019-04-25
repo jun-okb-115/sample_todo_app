a2226539893a1ed41350973471c5226c09c453d2
junya <jun.okb.115@gmail.com>
2019-04-25 10:33:04 +0800

    update

--- Todo/edit.php
+++ Todo/edit.php
@@ -2,12 +2,16 @@
 <html lang="ja">
 <head>
   <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
+  <title>編集</title>
 </head>
 <body>
-  <h1>これがedit.php</h1>
-  
+  <form action="" method="post">
+    <input type="hidden" name="id" value="<?= $_GET['id']; ?>">
+    <input type="text" name="todo" value="<?= $_GET['id']; ?>">
+    <input type="submit" value="更新">
+  </form>
+  <div>
+    <a href="index.php">一覧へもどる</a>
+  </div>
 </body>
 </html>
--- Todo/index.php
+++ Todo/index.php
@@ -2,12 +2,39 @@
 <html lang="ja">
 <head>
   <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
+  <title>Home</title>
 </head>
 <body>
-  <h1>これがindex.html</h1>
-  <a href="./edit.php">edit</a>
+  welcome hello world
+  <div>
+     <a href="new.php">
+       <p>新規作成</p>
+     </a>
+  </div>
+  <div>
+    <table>
+      <tr>
+        <th>ID</th>
+        <th>内容</th>
+        <th>更新</th>
+        <th>削除</th>
+      </tr>
+      <?php foreach (todoList() as $todo) : ?>
+        <tr>
+          <td><?= $todo['id']; ?></td>
+          <td><?= $todo['todo']; ?></td>
+          <td>
+            <a href="edit.php?id=<?= $todo['id']; ?>">更新</a>
+          </td>
+          <td>
+            <form action="store.php" method="post">
+              <input type="hidden" name="id" value="<?= $todo['id']; ?>">
+              <button type="submit">削除</button>
+            </form>
+          </td>
+        </tr>
+      <?php endforeach; ?>
+    </table>
+  </div>
 </body>
 </html>
